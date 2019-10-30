f6c5a89fbbc1db75a3c9ebc4ad05837a0911fbb7
Kulyba Vadim <kulyba.vadim@gmail.com>
2019-10-30 15:10:04 +0300

    feat(weather): implement weather api

--- src/App.js
+++ src/App.js
@@ -4,12 +4,22 @@ import Form from "./components/Form";
 import Titles from "./components/Titles";
 import Weather from "./components/Weather";
 
+const WEATHER_API_KEY = "d2ee401549c61995396bb55b409103a3";
+
 class App extends React.Component {
+  getWeather = async (event) => {
+    event.preventDefault();
+    const api_request = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=
+      Manchester,uk&appid=${WEATHER_API_KEY}&units=metric`);
+    const response_data = await api_request.json();
+    console.log(response_data);
+  }
+
   render() {
     return (
       <div>
         <Titles/>
-        <Form/>
+        <Form getWeather={this.getWeather}/>
         <Weather/>
       </div>
     );
