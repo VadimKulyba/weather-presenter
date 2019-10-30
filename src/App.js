97f92810afc657e7856d0783733fd2b45445dc68
Kulyba Vadim <kulyba.vadim@gmail.com>
2019-10-30 14:11:57 +0300

    feat(app): add main components templates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
 
+import Form from "./components/Form";
+import Titles from "./components/Titles";
+import Weather from "./components/Weather";
+
 class App extends React.Component {
   render() {
     return (
       <div>
+        <Titles/>
+        <Form/>
+        <Weather/>
       </div>
     );
   }
